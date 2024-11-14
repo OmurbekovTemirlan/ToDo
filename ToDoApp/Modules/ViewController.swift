744c844c9623a67bd180dcfa83df541af899e139
Tima <158593312+OmurbekovTemirlan@users.noreply.github.com>
2024-11-14 21:24:20 +0600

    initial commit

---
+++ ToDoApp/Modules/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Apple on 14.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
